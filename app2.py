be46e5dffe88b558d39140635493efbba673c225
Debajyoti <debajyotibindu1@gmail.com>
2026-02-24 15:48:23 +0530

    Added trransformer app

---
+++ app2.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from transformers import pipeline
+import os
+
+st.set_page_config(page_title="AI Text Generator", )
+
+st.title("AIText Generator")
+
+MODEL_PATH = r"D:\mlproject7\model_gpt2"
+
+@st.cache_resource  
+def load_sherlock():
+    if not os.path.exists(MODEL_PATH):
+        st.error(f"Model folder not found at {MODEL_PATH}")
+        return None
+    return pipeline("text-generation", model=MODEL_PATH)
+
+    sherlock_pipe = load_sherlock()
+
+prompt = st.text_input("Start a sentence for Sherlock:", "The mystery of the")
+
+col1, col2 = st.columns(2)
+with col1:
+    max_len = st.slider("Length of response", 20, 200, 50)
+with col2:
+    temp = st.slider("Creativity (Temperature)", 0.1, 1.0, 0.7)
+
+if st.button("Generate Logic"):
+    if sherlock_pipe:
+        with st.spinner("Loading"):
+            # Generate!
+            result = sherlock_pipe(
+                prompt, 
+                max_length=max_len, 
+                temperature=temp,
+                do_sample=True,
+                truncation=True
+            )
+            
+            st.subheader("Result:")
+            st.write(result[0]['generated_text'])
+    else:
+        st.warning("Please check your model path!")
